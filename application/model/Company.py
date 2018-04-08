3e84366fa9462747688d90deed7d48e6ad943683
Phil Goo Kang <philgookang@gmail.com>
2018-04-09 00:35:40 +0900

    added getList function for listing company's

--- application/model/Company.py
+++ application/model/Company.py
@@ -1,5 +1,6 @@
 from system.database.DataModel import DataModel
 from system.database.BusinessModel import BusinessModel
+from datetime import datetime
 
 class COMPANY_NEED_HISTORY:
     NO  = 1
@@ -13,12 +14,19 @@ class COMPANY_MARKET:
 
 class Company(DataModel, BusinessModel):
 
+    idx                 = None
     name                = None
     code                = None
     market              = None
     need_history        = None
     last_updated        = None
     created_date_time   = None
+
+    @staticmethod
+    def new(data = {}):
+        new = Company()
+        new.extend(data)
+        return new
 
     def create(self):
 
@@ -28,7 +36,7 @@ class Company(DataModel, BusinessModel):
         query +=    "( %s, %s, %s, %s, %s, %s, %s ) "
 
         return self.postman.create(query, [
-            name, code, market, COMPANY_NEED_HISTORY.YES, str(datetime.now().strftime("%Y-%m-%d %H:%I:%S")), str(datetime.now().strftime("%Y-%m-%d %H:%I:%S")), '1'
+            self.name, self.code, self.market, COMPANY_NEED_HISTORY.YES, str(datetime.now().strftime("%Y-%m-%d")), str(datetime.now().strftime("%Y-%m-%d %H:%I:%S")), '1'
         ])
 
     def get(self, select = ' idx,name,code '):
@@ -38,13 +46,52 @@ class Company(DataModel, BusinessModel):
         query += " FROM "
         query +=    "`company` "
         query += "WHERE "
-        if idx:     query += "`idx`=%s AND "
-        if name:    query += "`name`=%s AND "
+        if self.idx:     query += "`idx`=%s AND "
+        if self.name:    query += "`name`=%s AND "
         query +=    "`status`=%s "
 
         params = []
-        if idx:     params.append(idx)
-        if name:    params.append(name)
+        if self.idx:     params.append(self.idx)
+        if self.name:    params.append(self.name)
         params.append('1')
 
-        return self.extend(self.postman.get(query, params))
+        return Company.new(self.postman.get(query, params))
+
+    def getList(self, **kwargs):
+
+        sort_by     = kwargs['idx'] if kwargs['idx'] else 'idx'
+        sdirection  = kwargs['sort_by'] if kwargs['sort_by'] else 'desc'
+        limit       = kwargs['limit'] if kwargs['limit'] else 20
+        nolimit     = kwargs['nolimit'] if kwargs['nolimit'] else False
+        offset      = kwargs['offset'] if kwargs['offset'] else 0
+        select      = kwargs['select'] if kwargs['select'] else ' idx,name,code,market,need_history '
+
+        query  = "SELECT "
+        query +=    select
+        query += " FROM "
+        query +=    "`company` "
+        query += "WHERE "
+        if self.need_history:   query += "`need_history`=%s AND "
+        if self.market:         query += "`market`=%s AND "
+        if self.last_updated:   query += "`last_updated`<%s AND "
+        query +=    "`status`=%s "
+        query += "ORDER BY {0} {1} ".format(sort_by, sort_direction)
+        if not nolimit:         query += "LIMIT %s offset %s "
+
+        params = []
+        if self.need_history:   params.append(need_history)
+        if self.market:         params.append(market)
+        if self.last_updated:   params.append(last_updated)
+        params.append('1')
+        if not nolimit:         params.extend(limit, offset)
+
+        list        = self.postman.getList(query, params)
+        return_list = list(map(lambda x: Company.new(x), list))
+
+        return return_list
+
+    def updateNeedHistory(self):
+        self.postman.execute(
+            " UPDATE `company` SET `need_history`=%s WHERE `idx`=%s ",
+            [self.need_history, self.idx]
+        )
