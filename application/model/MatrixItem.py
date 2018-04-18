72e239d8bc7f1fbe4da46796e96bfa36a41f6242
Phil Goo Kang <philgookang@gmail.com>
2018-04-19 00:10:28 +0900

    added array to numpy matrix function

--- application/model/MatrixItem.py
+++ application/model/MatrixItem.py
@@ -1,6 +1,7 @@
 from system.database.DataModel import DataModel
 from system.database.BusinessModel import BusinessModel
 from datetime import datetime
+import numpy as np
 
 class MatrixItem(DataModel, BusinessModel):
 
@@ -21,6 +22,66 @@ class MatrixItem(DataModel, BusinessModel):
         new.extend(data)
         return new
 
+    @staticmethod
+    def converMatrix(match_list):
+
+        matrix_col = []
+
+        for item in match_list:
+
+            max     = item.max
+            min     = item.min
+            high    = item.high
+            low     = item.low
+
+            # the number of steps,
+            # increase this for more accurency, i think??????
+            scale   = 1000
+
+            # create steps in matrix
+            step   = max / scale;
+
+            # list that will hold the values
+            col = [];
+
+            # loop variables
+            cnt_i = 0
+
+            # loop through range to generate matrix
+            while(True):
+
+                if cnt_i >= max:
+                    break
+                else:
+                    cnt_i = cnt_i + step
+
+                lvl = (cnt_i * step);
+
+                if lvl >= low and lvl <= high:
+                    col.append(1)
+                else:
+                    col.append(0)
+
+            matrix_col.append(col)
+
+        ######################
+
+        if len(matrix_col) <= 0:
+            return np.array([])
+
+        new_matrix = []
+
+        for rowIdn in range(0, 1000):
+
+            row = []
+
+            for colIdn in range(len(matrix_col)):
+
+                row.append(matrix_col[colIdn][rowIdn])
+
+            new_matrix.append(row)
+
+        return np.array(new_matrix)
 
     def create(self):
 
@@ -55,3 +116,34 @@ class MatrixItem(DataModel, BusinessModel):
         params.append('1')
 
         return MatrixItem.new(self.postman.get(query, params))
+
+    def getList(self, **kwargs):
+
+        sort_by     = kwargs['sort_by']         if 'sort_by'        in kwargs else 'idx'
+        sdirection  = kwargs['sort_direction']  if 'sort_direction' in kwargs else 'desc'
+        limit       = kwargs['limit']           if 'limit'          in kwargs else 20
+        nolimit     = kwargs['nolimit']         if 'nolimit'        in kwargs else False
+        offset      = kwargs['offset']          if 'offset'         in kwargs else 0
+        select      = kwargs['select']          if 'select'         in kwargs else ' idx,matrix_idx,company_idx,company_stock_idx,col,max,min,high,low '
+
+        query  = "SELECT "
+        query +=    select
+        query += " FROM "
+        query +=    "`matrix_item` "
+        query += "WHERE "
+        if self.matrix_idx:     query += "`matrix_idx`=%s AND "
+        if self.company_idx:    query += "`company_idx`=%s AND "
+        query +=    "`status`=%s "
+        query += "ORDER BY {0} {1} ".format(sort_by, sdirection)
+        if not nolimit:         query += "LIMIT %s offset %s "
+
+        params = []
+        if self.matrix_idx:     params.append(self.matrix_idx)
+        if self.company_idx:    params.append(self.company_idx)
+        params.append('1')
+        if not nolimit:         params.extend((limit, offset))
+
+        sqllist     = self.postman.getList(query, params)
+        return_list = list(map(lambda x: MatrixItem.new(x), sqllist))
+
+        return return_list
