cda1c2222b9d303c552ae175daacf8ffb8b0c981
Phil Goo Kang <philgookang@gmail.com>
2018-04-26 15:12:39 +0900

    added amtrix compare date

---
+++ matrix_autoadd.py
@@ -0,0 +1,41 @@
+from urllib.parse import quote_plus, unquote_plus, urlparse, parse_qs
+from bs4 import BeautifulSoup
+import requests
+import sys
+from datetime import datetime
+from datetime import timedelta
+from system.Helper import progressbar
+from system.analytics.Stopwatch import Stopwatch
+from application.model.Matrix import MATRIX_PROCESSED
+from application.model.Matrix import Matrix
+
+class MatrixPreload:
+
+    def init(self):
+
+        # start stopwatch
+        stopwatch = Stopwatch.init()
+
+        # start time
+        stopwatch.start("company_history")
+
+        # loop through 60 days
+        for i in range(1, 3):
+
+            finddate = datetime.now()
+            finddate -= timedelta(days = i)
+
+            # get matrix list
+            matrix = Matrix.new({
+                "start_date"    : finddate.strftime("%Y-%m-%d"),
+                "end_date"      : str(datetime.now().strftime("%Y-%m-%d")),
+                "processed"     : MATRIX_PROCESSED.NO
+            })
+
+            check = matrix.get()
+
+            if not check.idx:
+                matrix.create()
+
+mp = MatrixPreload()
+mp.init()
