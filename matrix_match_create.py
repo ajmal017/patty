e0071518b22fae7f9d656d8b6274623ca99b6786
Phil Goo Kang <philgookang@gmail.com>
2018-04-27 16:09:29 +0900

    fixed asymmetric matrix size error

--- matrix_match_create.py
+++ matrix_match_create.py
@@ -14,6 +14,8 @@ from application.model.MatrixMatch import MatrixMatch
 from application.model.MatrixMatch import MATRIX_MATCH_PROCESSED
 from application.model.MatrixItem import MatrixItem
 from application.model.MatrixMatchItem import MatrixMatchItem
+from datetime import date
+from datetime import datetime
 
 class MatrixMatchCreate:
 
@@ -26,10 +28,16 @@ class MatrixMatchCreate:
         # change their status to processed
         for match in match_list:
             match.processed = MATRIX_MATCH_PROCESSED.YES
-            match.updateProcessed()
+            # match.updateProcessed()
 
         # loop through match list
         for match in match_list:
+
+            check_matrix_time = Matrix.new({ "idx" : match.matrix_idx }).get( " start_date,end_date " )
+
+            start_date  = datetime.strptime(str(check_matrix_time.start_date), "%Y-%m-%d")
+            end_date    = datetime.strptime(str(check_matrix_time.end_date), "%Y-%m-%d")
+            days_count  = abs((end_date - start_date).days)
 
             # current company
             current_matrix = MatrixItem.new({
@@ -37,7 +45,7 @@ class MatrixMatchCreate:
                 "company_idx"   : match.company_idx
             })
             main_matrix_item_list = current_matrix.getList(sort_by = 'col', sort_direction = 'asc', nolimit = True, select = ' idx,max,min,high,low ')
-            matrix_main = MatrixItem.converMatrix(main_matrix_item_list)
+            matrix_main = MatrixItem.converMatrix(main_matrix_item_list, days_count)
 
             for idx in range(1, 4000):
 
@@ -53,7 +61,7 @@ class MatrixMatchCreate:
                 if len(sub_matrix_item_list) <= 0:
                     continue
 
-                matrix_sub = MatrixItem.converMatrix(sub_matrix_item_list)
+                matrix_sub = MatrixItem.converMatrix(sub_matrix_item_list, days_count)
 
                 abs_matrix = np.absolute(matrix_main - matrix_sub)
 
