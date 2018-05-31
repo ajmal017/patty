ac9e2ffcc4640d637a9deebee7e5a94b25ff9df7
Phil Goo Kang <philgookang@gmail.com>
2018-06-01 01:18:19 +0900

    added PlaylistFindTopVolume class to init load

--- component/__init__.py
+++ component/__init__.py
@@ -1,3 +1,7 @@
+# company
 from component.CompanyDaily import CompanyDaily
 from component.CompanySearch import CompanySearch
 from component.CompanyHistory import CompanyHistory
+
+# playlist
+from component.PlaylistFindTopVolume import PlaylistFindTopVolume
