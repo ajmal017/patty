d61846ea1d6ac98e1edd66326ec1d3d997ccd450
Phil Goo Kang <philgookang@gmail.com>
2018-06-15 02:00:42 +0900

    finished refactoring calender class

--- website/application/helper/Calendar.php
+++ website/application/helper/Calendar.php
@@ -18,51 +18,38 @@ class Calendar {
         $this->month    = $month;
         $this->year     = $year;
 
-
+        return $this->create_view();
     }
 
-    public function getDaysInMonth() {
+    public function getDaysInMonth($month, $year) {
 
-        if (($this->month == 9) || ($this->month == 4) ||
-			($this->month == 6) || ($this->month == 11) ) {
+        if (($month == 9) || ($month == 4) ||
+			($month == 6) || ($month == 11) ) {
 			return 30;
 		}
 
 		if (($this->month != 2)) { return 31; }
 
-		if ( ( (($this->year % 4) == 0) && (($this->year % 100) != 0) ) ||
-			 ( ($this->year % 400) == 0 )) {
+		if ( ( (($year % 4) == 0) && (($year % 100) != 0) ) ||
+			 ( ($year % 400) == 0 )) {
 			return 29;
 		}
 
 		return 28;
     }
 
-    public function getFrontBackMonth() {
-
-    	if ($this->month == 1) {
-    		$pre_month = '12';
-    		$pre_year = ($this->year - 1);
-    	} else {
-    		$pre_month = ($this->month - 1);
-    		$pre_year = $this->year;
+    public function getPrevDate() {
+        if ($this->month == 1) {
+            return array('12', ($this->year - 1));
     	}
+        return array(($this->month - 1), $this->year);
+    }
 
-    	if ($this->month == 12) {
-    		$next_month = '1';
-    		$next_year = ($this->year + 1);
-    	} else {
-    		$next_month = ($this->month + 1);
-    		$next_year = $this->year;
-    	}
-
-    	$preview_month_day = days_in_month($pre_month, $pre_year);
-    	$current_month_day = days_in_month($month, $year);
-
-    	$pre_month = ($pre_month > 9) ? $pre_month : '0'.$pre_month;
-    	$next_month = ($next_month > 9) ? $next_month : '0'.$next_month;
-
-    	$start_day = ((int)date('w', strtotime($month . '/01/' . $year )) + 1);
+    public function getNextDate() {
+        if ($this->month == 12) {
+            return array('1', ($this->year + 1));
+        }
+        return array(($this->month + 1), $this->year);
     }
 
     public function getWeekLabel() {
@@ -73,49 +60,36 @@ class Calendar {
         }
     }
 
-    private function day_view($date, $another_month, $options) {
-        $html  = "</div>";
-        $html .= "</div>";
-        return $html;
-    }
-
     public function create_view() {
-        $html = '';
 
+        // date list
+        $date_list = array();
 
-        $html .= '<div class="auto-cal">';
-        $html .= '<table>';
-        $html .= '<thead>';
-        foreach($this->getWeekLabel() as $label) {
-            $html .= '<th>$label</th>';
-        }
-        $html .= '</thead>';
-        $html .= '<tbody>';
+        list($pre_month, $pre_year) = $this->getPrevDate();
+        list($next_month, $next_year) = $this->getNextDate();
+
+    	$preview_month_day = $this->getDaysInMonth($pre_month, $pre_year);
+    	$current_month_day = $this->getDaysInMonth($this->month, $this->year);
+
+    	$pre_month = ($pre_month > 9) ? $pre_month : '0'.$pre_month;
+    	$next_month = ($next_month > 9) ? $next_month : '0'.$next_month;
+
+    	$start_day = ((int)date('w', strtotime($this->month . '/01/' . $this->year )) + 1);
 
         for($i = 1, $j = 1; $i <= 42; $i++) {
-            if ( ($i == 0) || ($i == 8) || ($i == 15) || ($i == 22) || ($i == 29) || ($i == 36) ) {
-                $html .= '<tr>';
-            }
             if ($i < $start_day) {
-                $html .= $this->day_view( ($preview_month_day - ($start_day - $i) + 1), 'another-month', '0', '0' );
+                array_push($date_list, $pre_year.'-'.$pre_month.'-'.($preview_month_day - ($start_day - $i) + 1));
             } else if (($i - $start_day + 1) <= $current_month_day) {
-                $html .= $this->day_view( $day_orgn, '', array(
-                    'signup'		=> $signup,
-                    'purchase_cnt'	=> count($today_purchase),
-                    'total_cost'	=> $total_cost
-                ));
+                $day_orgn = ($i - $start_day + 1);
+				$day_orgn2 = ($day_orgn<10) ? '0'.$day_orgn : $day_orgn;
+                array_push($date_list, $this->year.'-'.$this->month.'-'.$day_orgn2);
             } else {
-                $html .= $this->day_view( $j++, 'another-month', '0', '0' );
-            }
-            if ( ($i == 7) || ($i == 14) || ($i == 21) || ($i == 28) || ($i == 35) || ($i == 42) ){
-                $html .= '</tr>';
+                $d = ($j >= 10) ? $j : '0'.$j;
+                $j++;
+                array_push($date_list, $next_year.'-'.$next_month.'-'.$d);
             }
         }
 
-        $html .= '</tbody>';
-        $html .= '</table>';
-        $html .= '</div>';
-        $html .= '<!--/.auto-cal-->';
-        return $html;
+        return $date_list;
     }
 }
