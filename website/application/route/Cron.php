57cf0f426f98582a44ba6a0b957ac7cd1344188d
Phil Goo Kang <philgookang@gmail.com>
2018-06-14 23:02:37 +0900

    removed old matrix class

--- website/application/route/Cron.php
+++ website/application/route/Cron.php
@@ -4,10 +4,8 @@ Map::path('cron', function() {
 
     $data                                       = array();
     $data['company_daily_update_waiting_count'] = CompanyM::new()->getDailyUpdateWaitingCount();
-    $data['matrix_not_processed_count']         = MatrixM::new()->getNotProcessedCount();
-    $data['matrix_processed_count']             = MatrixM::new()->getProcessedCount();
-    $data['matrix_match_not_processed_count']   = MatrixMatchM::new()->getNotProcessedCount();
-    $data['matrix_match_processed_count']       = MatrixMatchM::new()->getProcessedCount();
+    $data['company_history_count']              = CompanyM::new()->getNeedHistoryCount();
+
 
     $this->load->html('template/head');
     $this->load->html('page/cron/index', $data);
