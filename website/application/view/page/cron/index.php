7c1ccadb1e545f78dcba81cafdf7c127e10caaf5
Phil Goo Kang <philgookang@gmail.com>
2018-06-14 23:03:05 +0900

    updated text for status

--- website/application/view/page/cron/index.php
+++ website/application/view/page/cron/index.php
@@ -1,39 +1,23 @@
 <script type="text/javascript" src="/public/js/vendor/jquery.easypiechart.min.js"></script>
 
 <div class="row">
-    <h1>상위 100 거래량</h1>
+    <h1>처리 상태</h1>
 </div>
 <!--/.row-->
 
 <div class="row col">
     <div class="col-3">
-        <div class="line-title">상위 100 거래량</div>
+        <div class="line-title">매일 업데이트</div>
         <!--/.line-title-->
-        <div class="pie-graph" data-percent="39"><span class="pie-value text-thin text-2x"></span></div>
+        <div class="pie-graph" data-percent="<?php echo $company_daily_update_waiting_count; ?>"><span class="pie-value text-thin text-2x"></span></div>
         <!--/.pie-graph-->
     </div>
     <!--/.col-3-->
 
     <div class="col-3">
-        <div class="line-title">상위 100 거래량</div>
+        <div class="line-title">신규등록 처리 대기</div>
         <!--/.line-title-->
-        <div class="pie-graph" data-percent="39"><span class="pie-value text-thin text-2x"></span></div>
-        <!--/.pie-graph-->
-    </div>
-    <!--/.col-3-->
-
-    <div class="col-3">
-        <div class="line-title">상위 100 거래량</div>
-        <!--/.line-title-->
-        <div class="pie-graph" data-percent="39"><span class="pie-value text-thin text-2x"></span></div>
-        <!--/.pie-graph-->
-    </div>
-    <!--/.col-3-->
-
-    <div class="col-3">
-        <div class="line-title">상위 100 거래량</div>
-        <!--/.line-title-->
-        <div class="pie-graph" data-percent="39"><span class="pie-value text-thin text-2x"></span></div>
+        <div class="pie-graph" data-percent="<?php echo $company_history_count; ?>"><span class="pie-value text-thin text-2x"></span></div>
         <!--/.pie-graph-->
     </div>
     <!--/.col-3-->
