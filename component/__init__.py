870785ca35552f805054608008df1c3f8ded08f9
Phil Goo Kang <philgookang@gmail.com>
2018-06-18 15:19:32 +0900

    added custom playlist process class

--- component/__init__.py
+++ component/__init__.py
@@ -4,5 +4,6 @@ from component.CompanySearch import CompanySearch
 from component.CompanyHistory import CompanyHistory
 
 # playlist
+from component.PlaylistCustomSVM import PlaylistCustomSVM
 from component.PlaylistFindTopVolume import PlaylistFindTopVolume
 from component.PlaylistTopSVM import PlaylistTopSVM
