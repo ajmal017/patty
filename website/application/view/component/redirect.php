0a5ddff25d9b76bdbda4c462d3a0246456647faa
Phil Goo Kang <philgookang@gmail.com>
2018-06-18 16:08:14 +0900

    added view based redirect view

---
+++ website/application/view/component/redirect.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1;url=<?php echo $url; ?>">
+
+		<title></title>
+
+		<script type="text/javascript">
+			alert("<?php echo $msg; ?>");
+            window.location.href = "<?php echo $url; ?>";
+        </script>
+	</head>
+	<body></body>
+</html>
