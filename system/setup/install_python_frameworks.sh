6f696727937fec629fa55a4cd2b623484d830e4a
Phil Goo Kang <philgookang@gmail.com>
2018-06-22 00:23:34 +0900

    added additional required library for project

--- system/setup/install_python_frameworks.sh
+++ system/setup/install_python_frameworks.sh
@@ -1,5 +1,6 @@
-
-apt-get install python3-pandas
-apt-get install python3-scipy
-apt-get install python3-numpy
-pip3 install -U scikit-learn
+sudo apt-get install python3-pip
+sudo apt-get install python3-pandas
+sudo apt-get install python3-scipy
+sudo apt-get install python3-numpy
+sudo pip3 install -U scikit-learn
+sudo pip3 install mysql-connector
