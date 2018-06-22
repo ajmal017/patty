7b1bd1d48aee621474719d554611249116b30cb3
Phil Goo Kang <philgookang@gmail.com>
2018-06-22 23:44:44 +0900

    added dsformat function to load

--- tool/__init__.py
+++ tool/__init__.py
@@ -4,4 +4,5 @@ from tool.Stopwatch import Stopwatch
 # -- func
 from tool.Helper import progressbar
 from tool.Helper import dformat
+from tool.Helper import dsformat
 from tool.Helper import getDateTime
