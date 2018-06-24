25f299aa360e18520b0dcb88c63ac61dde4d3762
Phil Goo Kang <philgookang@gmail.com>
2018-06-24 18:23:53 +0900

    adeed json processing components

--- component/__init__.py
+++ component/__init__.py
@@ -6,6 +6,8 @@ from component.CompanyHistory import CompanyHistory
 # playlist
 from component.PlaylistProcessSVM import PlaylistProcessSVM
 from component.PlaylistFindTopVolume import PlaylistFindTopVolume
+from component.PlaylistToFile import PlaylistToFile
+from component.PlaylistJsonSolve import PlaylistJsonSolve
 
 #  watch
 from component.WatchToPlaylist import WatchToPlaylist
