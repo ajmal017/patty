91dc043af1bd00a76f00cd23d1edc6115ae24372
Phil Goo Kang <philgookang@gmail.com>
2018-06-24 18:25:01 +0900

    added new json solve files

---
+++ jsonsolve.py
@@ -0,0 +1,12 @@
+import sys
+from component import *
+
+def dump():
+    PlaylistToFile().init()
+
+def solve():
+    PlaylistJsonSolve().init()
+
+"""check if the function that needs to be called has been passed """
+if len(sys.argv) >= 2:
+    locals()[sys.argv[1]]()
