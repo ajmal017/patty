a5ba0b74d0b2999deb8522e86dde973a2f02df95
Phil Goo Kang <philgookang@gmail.com>
2018-06-27 05:01:07 +0900

    added multicore processing SVM

--- component/PlaylistProcessSVM.py
+++ component/PlaylistProcessSVM.py
@@ -1,10 +1,13 @@
 from database import *
 from tool import *
 from model import *
+from multiprocessing import Pool
 
 class PlaylistProcessSVM:
 
-    def get_company_list(self, skip_company_idx):
+    company_list = []
+
+    def get_company_list(self, skip_company_idx = []):
 
         # main company list
         company_list = []
@@ -29,82 +32,80 @@ class PlaylistProcessSVM:
                 company_list = company_list + t
 
         # return main company list
-        return company_list
+        self.company_list = company_list
 
 
-    def init(self):
+    def initMultiCore(self):
+        playlist_list = self.preprocess();
+        with Pool as p:
+            list(p.map(self.mainprocess, sleep_time_list))
 
-        playlist = Playlist.new()
-        playlist.svm_processed  = PLAYLIST_PROCESS.WAIT
+    def initSingleCore(self):
+        playlist_list = self.preprocess();
+        list(map(self.mainprocess, playlist_list))
 
-        # ----
+    def preprocess(self):
 
-        playlist_list       = playlist.getList(sort_by = "date", sort_direction = "desc", limit = 2, select = " idx,company_idx,date ")
-        skip_company_idx    = []
+        self.get_company_list()
+
+        playlist_list = Playlist.new({
+            "svm_processed":PLAYLIST_PROCESS.WAIT
+        }).getList(sort_by = "date", sort_direction = "desc", limit = 3, select = " idx,company_idx,date ")
 
         for playlist in playlist_list:
-            skip_company_idx.append(playlist.company_idx)
             playlist.svm_processed = PLAYLIST_PROCESS.PROCESS
             playlist.update_svm_process()
 
-        # ---
+        return playlist_list
 
-        size = 100
+    def mainprocess(self, playlist):
 
-        # ---
+        # set update date time for svm weight
+        playlist.svm_processed_wait = getDateTime()
+        playlist.update_svm_processed_wait()
 
-        company_list = self.get_company_list(skip_company_idx)
+        train_stock_list = CompanyStock.new({
+            "company_idx"       : playlist.company_idx,
+            "search_start_date" : dsformat(str(playlist.date), size),
+            "search_end_date"   : dsformat(str(playlist.date))
+        }).getList(sort_by = 'date', sort_direction = 'desc', nolimit = True)
 
-        # ---
+        svm_model = SVMWrapper()
+        svm_model.train_data_x = CompanyStock.getOCHLV(train_stock_list, size)
+        svm_model.train_data_y = CompanyStock.getP(train_stock_list, size)
+        svm_model.train()
 
-        for playlist in playlist_list:
+        for company in company_list:
 
-            # set update date time for svm weight
-            playlist.svm_processed_wait = getDateTime()
-            playlist.update_svm_processed_wait()
+            if company.idx == playlist.company_idx:
+                continue
 
-            train_stock_list = CompanyStock.new({
-                "company_idx"       : playlist.company_idx,
+            predict_stock_list = CompanyStock.new({
+                "company_idx"       : company.idx,
                 "search_start_date" : dsformat(str(playlist.date), size),
                 "search_end_date"   : dsformat(str(playlist.date))
             }).getList(sort_by = 'date', sort_direction = 'desc', nolimit = True)
 
-            svm_model = SVMWrapper()
-            svm_model.train_data_x = CompanyStock.getOCHLV(train_stock_list, size)
-            svm_model.train_data_y = CompanyStock.getP(train_stock_list, size)
-            svm_model.train()
+            svm_model.test_data_x = CompanyStock.getOCHLV(predict_stock_list, size)
+            svm_model.test_data_y = CompanyStock.getP(predict_stock_list, size)
+            score = svm_model.test()
 
-            for company in company_list:
+            ModelResult.new({
+                "playlist_idx"      : playlist.idx,
+                "train_company_idx" : playlist.company_idx,
+                "test_company_idx"  : company.idx,
+                "f1"                : "0",
+                "accuracy"          : "0",
+                "recall"            : "0",
+                "precise"           : "0",
+                "score"             : float(score),
+                "type"              : MODEL_TYPE.SVM,
+                "duration"          : size
+            }).checkCreate()
 
-                if company.idx == playlist.company_idx:
-                    continue
+        playlist.svm_processed = PLAYLIST_PROCESS.DONE
+        playlist.update_svm_process()
 
-                predict_stock_list = CompanyStock.new({
-                    "company_idx"       : company.idx,
-                    "search_start_date" : dsformat(str(playlist.date), size),
-                    "search_end_date"   : dsformat(str(playlist.date))
-                }).getList(sort_by = 'date', sort_direction = 'desc', nolimit = True)
-
-                svm_model.test_data_x = CompanyStock.getOCHLV(predict_stock_list, size)
-                svm_model.test_data_y = CompanyStock.getP(predict_stock_list, size)
-                score = svm_model.test()
-
-                ModelResult.new({
-                    "playlist_idx"      : playlist.idx,
-                    "train_company_idx" : playlist.company_idx,
-                    "test_company_idx"  : company.idx,
-                    "f1"                : "0",
-                    "accuracy"          : "0",
-                    "recall"            : "0",
-                    "precise"           : "0",
-                    "score"             : float(score),
-                    "type"              : MODEL_TYPE.SVM,
-                    "duration"          : size
-                }).checkCreate()
-
-            playlist.svm_processed = PLAYLIST_PROCESS.DONE
-            playlist.update_svm_process()
-
-            # set update date time for svm weight
-            playlist.svm_processed_complete = getDateTime()
-            playlist.update_svm_processed_complete()
+        # set update date time for svm weight
+        playlist.svm_processed_complete = getDateTime()
+        playlist.update_svm_processed_complete()
