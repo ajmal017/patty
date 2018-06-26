7013e908631e05957cb3e7955a2d45d53cf95cfd
Phil Goo Kang <philgookang@gmail.com>
2018-06-27 06:15:59 +0900

    added multicore postman swithcing functino

--- system/database/DataModel.py
+++ system/database/DataModel.py
@@ -6,3 +6,8 @@ class DataModel:
 
     def __init__(self):
         self.postman = Postman.init()
+
+    def multicore(self, new_postman, multicore_status):
+        if multicore_status:
+            self.postman = new_postman
+        return self
