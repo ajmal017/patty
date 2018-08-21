d669a79fef8a9ce9b97f7bb342880c74ef8e3df2
Phil Goo Kang <philgookang@gmail.com>
2018-08-21 14:05:02 +0900

    removed unsed(deleted) components

--- component/__init__.py
+++ component/__init__.py
@@ -6,9 +6,6 @@ from component.CompanyHistory import CompanyHistory
 # playlist
 from component.PlaylistProcessSVM import PlaylistProcessSVM
 from component.PlaylistFindTopVolume import PlaylistFindTopVolume
-from component.PlaylistToFile import PlaylistToFile
-from component.PlaylistJsonSolve import PlaylistJsonSolve
-from component.ModelReadMatch import ModelReadMatch
 
 #  watch
 from component.WatchToPlaylist import WatchToPlaylist
